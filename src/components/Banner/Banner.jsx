336509dd14d97e66ae90fbdb44f3730fa5bd903f
Yicel Gutierrez <yicelgutierrezguillen@gmnail.com>
2024-11-28 20:21:00 -0500

    add component searh

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,24 @@
-import banner from "../../assets/banner_Pinky_Pink_Cosmetics.jpg";
 import { Search } from "../Search/Search";
 import "./banner.css";
+import banner from "../../assets/banner_Pinky_Pink_Cosmetics.jpg";
 
 export const Banner = () => {
   return (
-    <div className="banner_container">
-      <Search />
-
-      <img
-        src={banner}
-        alt="Banner promocional de Pinky Pink Cosmetics"
-        style={{
-          width: "100%",
-          height: "300px",
-          opacity: 0.3,
-        }}
-      />
+    <div className="container_banner">
+      <div className="banner_img">
+        <div className="search">
+          <Search />
+        </div>
+        <img
+          src={banner}
+          alt="Banner promocional de Pinky Pink Cosmetics"
+          style={{
+            opacity: 0.3,
+            width: "100%",
+            height: "300px",
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import searchIcon from "../../assets/search-24.png";
 import "./search.css";
 
 export const Search = () => {
   return (
-    <div>
+    <div className="search">
       <input
-        className=" search"
+        className="search_input"
         type="text"
         name="search"
-        placeholder="Escribe aqui tu producto de interes"
+        placeholder="Escribe aqui tu producto de interes..."
       />
+      <img src={searchIcon} className="search_img" />
     </div>
   );
 };
